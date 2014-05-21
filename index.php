0e6660c5b5295a0f8819ab1d3b72a14977a8acdb
Fernando Bossini <fernandobossini@gmail.com>
2014-05-21 14:04:01 -0300

    Adicionado um comentario

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+//Essa linha mostra o Hello World
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
